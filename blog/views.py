b95585176d632ff71763e3be22e156554439d16a
Your Name <you@example.com>
2016-11-19 02:10:20 +0100

    "Modified templates to display posts from database."

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,6 @@ from .models import Post
 from django.utils import timezone
 # Create your views here.
 def post_list(request):
+    # Shows only 4 recently updated posts.
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')[:4]
     return render(request   , 'blog/post_list.html', {'posts':posts})
